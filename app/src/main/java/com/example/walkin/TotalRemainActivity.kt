e1e09897dc8024f60b7107cc9e7e2d851bd802d7
panya naknoppakun <panya.naknoppakun@gmail.com>
2020-07-22 14:51:13 +0700

    Update function print

--- app/src/main/java/com/example/walkin/TotalRemainActivity.kt
+++ app/src/main/java/com/example/walkin/TotalRemainActivity.kt
@@ -2,16 +2,22 @@ package com.example.walkin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.RemoteException
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.centerm.smartpos.aidl.printer.AidlPrinter
+import com.centerm.smartpos.aidl.sys.AidlDeviceManager
+import com.centerm.smartpos.constant.Constant
 import com.example.walkin.models.PartialVisitorResponseModel
 import com.example.walkin.models.WalkInErrorModel
 import com.example.walkin.utils.NetworkUtil
 import kotlinx.android.synthetic.main.activity_detail.*
 
-class TotalRemainActivity : AppCompatActivity() {
+class TotalRemainActivity : BaseActivity() {
     lateinit var adapter : DetailAdapter
+    private var printDev: AidlPrinter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
@@ -27,7 +33,27 @@ class TotalRemainActivity : AppCompatActivity() {
         loadData()
     }
 
-    fun loadData() {
+
+    override fun onPrintDeviceConnected(manager: AidlDeviceManager?) {
+        try {
+            printDev = AidlPrinter.Stub.asInterface(manager!!.getDevice(Constant.DEVICE_TYPE.DEVICE_TYPE_PRINTERDEV))
+            adapter?.let {
+                it.setPrinter(printDev)
+            }
+        } catch (e: RemoteException) {
+            e.printStackTrace()
+        }    }
+
+    override fun onDeviceConnected(deviceManager: AidlDeviceManager?) {
+    }
+
+    override fun onDeviceConnectedSwipe(manager: AidlDeviceManager?) {
+    }
+
+    override fun showMessage(str: String?, black: Int) {
+    }
+
+        fun loadData() {
         NetworkUtil.getListByType(NetworkUtil.STATUS_TYPE_STAY, object : NetworkUtil.Companion.NetworkLisener<List<PartialVisitorResponseModel>> {
             override fun onResponse(response: List<PartialVisitorResponseModel>) {
                 adapter.setListdata(response)
--- app/src/main/java/com/example/walkin/TotalVisitorActivity.kt
+++ app/src/main/java/com/example/walkin/TotalVisitorActivity.kt
@@ -2,16 +2,22 @@ package com.example.walkin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.RemoteException
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.centerm.smartpos.aidl.printer.AidlPrinter
+import com.centerm.smartpos.aidl.sys.AidlDeviceManager
+import com.centerm.smartpos.constant.Constant
 import com.example.walkin.models.PartialVisitorResponseModel
 import com.example.walkin.models.WalkInErrorModel
 import com.example.walkin.utils.NetworkUtil
 import kotlinx.android.synthetic.main.activity_detail.*
 
-class TotalVisitorActivity : AppCompatActivity() {
+class TotalVisitorActivity : BaseActivity() {
     lateinit var adapter : DetailAdapter
+    private var printDev: AidlPrinter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
@@ -25,6 +31,28 @@ class TotalVisitorActivity : AppCompatActivity() {
             loadData()
         }
         loadData()
+    }
+
+
+
+    override fun onPrintDeviceConnected(manager: AidlDeviceManager?) {
+        try {
+            printDev = AidlPrinter.Stub.asInterface(manager!!.getDevice(Constant.DEVICE_TYPE.DEVICE_TYPE_PRINTERDEV))
+            adapter?.let {
+                it.setPrinter(printDev)
+            }
+        } catch (e: RemoteException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onDeviceConnected(deviceManager: AidlDeviceManager?) {
+    }
+
+    override fun onDeviceConnectedSwipe(manager: AidlDeviceManager?) {
+    }
+
+    override fun showMessage(str: String?, black: Int) {
     }
 
     fun loadData() {
