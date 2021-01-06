462123d172179543ab7aa4fcf2134b869e69f720
panya naknoppakun <panya.naknoppakun@gmail.com>
2021-01-06 10:03:02 +0700

    Update

--- app/src/main/java/com/example/walkin/cyp/CheckInActivity.java
+++ app/src/main/java/com/example/walkin/cyp/CheckInActivity.java
@@ -82,6 +82,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
--- app/src/main/java/com/example/walkin/cyp/app/WalkinApplication.kt
+++ app/src/main/java/com/example/walkin/cyp/app/WalkinApplication.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import com.example.walkin.cyp.utils.PreferenceUtils
 
 class WalkinApplication: Application() {
-
+//    var mReadCardOptV2: ReadCardOptV2? = null
     override fun onCreate() {
         super.onCreate()
         PreferenceUtils.init(applicationContext)
